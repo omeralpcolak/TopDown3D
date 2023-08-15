3e383d5dbddbc35bfb38c55c81acf34c0dac95df
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2023-08-15 13:24:06 +0300

    Enemy movement with navmesh

---
+++ TopDown3D/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Transform player;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        navMeshAgent.SetDestination(player.position);
+    }
+}
+
