4d900d346efbb581f16f30f025567f68358d81e3
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2023-10-08 13:10:31 +0300

    Equipping items

--- TopDown3D/Assets/Scripts/GameManager/Shop/OnSale.cs
+++ TopDown3D/Assets/Scripts/GameManager/Shop/OnSale.cs
@@ -6,15 +6,19 @@ using TMPro;
 public class OnSale : MonoBehaviour
 {
     public string itemName;
+
     public int itemPrice;
+
+    public GameObject itemObj;
 
     public TMP_Text itemNameTxt;
     public TMP_Text itemPriceTxt;
 
-    public void Initialize(string itemName, int itemPrice)
+    public void Initialize(string itemName, int itemPrice, GameObject itemObj)
     {
         this.itemName = itemName;
         this.itemPrice = itemPrice;
+        this.itemObj = itemObj;
 
         UpdatePrices();
     }
--- TopDown3D/Assets/Scripts/GameManager/Shop/ShopManager.cs
+++ TopDown3D/Assets/Scripts/GameManager/Shop/ShopManager.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-
     public int killCount;
     public int wallet;
 
     public OnSale vikingHat;
     public OnSale wizardHat;
     public OnSale sleepingHat;
+    public OnSale mexicanHat;
+
+    public GameObject vikingHatObj;
+    public GameObject wizardHatObj;
+    public GameObject sleepingHatObj;
+    public GameObject mexicanHatObj;
+
+    private GameObject equippedObj; 
+
+    private const string EquippedItemKey = "EquippedItem";
 
     void Start()
     {
         wallet = PlayerPrefs.GetInt("Wallet", 0);
 
-        vikingHat.Initialize("Viking Hat", 150);
-        wizardHat.Initialize("Wizard Hat", 300);
-        sleepingHat.Initialize("Sleeping Hat", 10);
+        vikingHat.Initialize("Viking Hat", 150, vikingHatObj);
+        wizardHat.Initialize("Wizard Hat", 300, wizardHatObj);
+        sleepingHat.Initialize("Sleeping Hat", 10, sleepingHatObj);
+        mexicanHat.Initialize("Sombrero", 0, mexicanHatObj);
+
+        string equippedItemName = PlayerPrefs.GetString(EquippedItemKey, "");
+        if (!string.IsNullOrEmpty(equippedItemName))
+        {
+            OnSale equippedItem = FindItemByName(equippedItemName);
+            if (equippedItem != null)
+            {
+                EquipItem(equippedItem);
+            }
+        }
     }
 
     public void GainSoul()
     {
-
         killCount++;
         wallet += killCount;
         killCount = 0;
@@ -35,12 +54,55 @@ public class ShopManager : MonoBehaviour
         wallet -= item.itemPrice;
         SaveWallet();
         ScreenManager.instance.UpdateTexts();
-
+        UnequipItem();
+        EquipItem(item);
+        PlayerPrefs.SetString(EquippedItemKey, item.itemName);
+        PlayerPrefs.Save();
     }
 
     public void SaveWallet()
     {
         PlayerPrefs.SetInt("Wallet", wallet);
         PlayerPrefs.Save();
+    }
+
+    private void EquipItem(OnSale item)
+    {
+        if (item.itemObj != null)
+        {
+            item.itemObj.SetActive(true);
+            equippedObj = item.itemObj;
+        }
+    }
+
+    private void UnequipItem()
+    {
+        if (equippedObj != null)
+        {
+            equippedObj.SetActive(false);
+            equippedObj = null;
+        }
+    }
+
+    private OnSale FindItemByName(string itemName)
+    {
+        if(itemName == mexicanHat.itemName)
+        {
+            return mexicanHat;
+        }
+
+        if (itemName == vikingHat.itemName)
+        {
+            return vikingHat;
+        }
+        else if (itemName == wizardHat.itemName)
+        {
+            return wizardHat;
+        }
+        else if (itemName == sleepingHat.itemName)
+        {
+            return sleepingHat;
+        }
+        return null;
     }
 }
